1a52beea1a423814b9c844dd5e7d8a49bfb490ff
CM_CHENGCHUANPENG <chengchuanpeng@cmcm.com>
2017-08-29 20:39:08 +0800

    first cm

---
+++ SioClientTLS/SioClientTLS.h
@@ -0,0 +1,19 @@
+//
+//  SioClientTLS.h
+//  SioClientTLS
+//
+//  Created by cheng chuanpeng on 25/08/2017.
+//  Copyright © 2017 cheng chuanpeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SioClientTLS.
+FOUNDATION_EXPORT double SioClientTLSVersionNumber;
+
+//! Project version string for SioClientTLS.
+FOUNDATION_EXPORT const unsigned char SioClientTLSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SioClientTLS/PublicHeader.h>
+
+
---
+++ SioClientTLSTests/SioClientTLSTests.m
@@ -0,0 +1,39 @@
+//
+//  SioClientTLSTests.m
+//  SioClientTLSTests
+//
+//  Created by cheng chuanpeng on 25/08/2017.
+//  Copyright © 2017 cheng chuanpeng. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SioClientTLSTests : XCTestCase
+
+@end
+
+@implementation SioClientTLSTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
